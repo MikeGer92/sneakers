b170f5fc224a7334462b2bd5848410a573140e86
MikeGer <mikegera@yandex.ru>
2024-03-13 15:54:07 +0300

    added Card

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <script setup>
 import Header from './components/icons/Header.vue'
+import Card from './components/Card.vue'
 </script>
 
 <template>
-  <div class="bg-white w-4/5 h-screen m-auto rounded-xl shadow-xl mt-14">
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header></Header>
-  </div>
-
-  <div>
-    <div>
-      <img src="/like_1.svg" alt="Like-1" />
-    </div>
+    <Card />
   </div>
 </template>
 
