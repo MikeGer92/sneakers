e917770cd4891b5b827001af07d531a8765a905b
MikeGer <mikegera@yandex.ru>
2024-03-13 14:55:53 +0300

    added Header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
+<script setup>
+import Header from './components/icons/Header.vue'
+</script>
+
 <template>
-  <h1>Hello</h1>
-  <h3 class="text-3xl font-bold underline">Sneakers</h3>
-  <MyHello />
+  <div class="bg-white w-4/5 h-screen m-auto rounded-xl shadow-xl mt-14">
+    <Header></Header>
+  </div>
+
+  <div>
+    <div>
+      <img src="/like_1.svg" alt="Like-1" />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/MyHello.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <p>Это мой компонент</p>
-  <button>Открыть</button>
-</template>
-
-<style scoped lang="scss"></style>
